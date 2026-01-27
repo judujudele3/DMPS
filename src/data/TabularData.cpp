0cc1482c6745c5ab8ffb9a89bb6d10ae6636d228
Mohamed_Klila <mohamed.klila@etudiant-enit.utm.tn>
2026-01-27 19:17:06 +0100

    merge

--- src/data/TabularData.cpp
+++ src/data/TabularData.cpp
@@ -21,9 +21,11 @@ const std::vector<std::string>& TabularData::header() const {
     return m_header;
 }
 
+
 void TabularData::addRow(const std::vector<std::string>& row) {
     m_rows.push_back(row);
 }
+
 
 //constant version
 const std::vector<std::vector<std::string>>& TabularData::rows() const {
